bdbb1efc0837c3e18f30a491e11b77fe5f8737ef
zhangxiaojun00 <1982388675@qq.com>
2020-01-10 20:37:31 +0800

    P2P框架

---
+++ src/main/java/com/baby/BabyP2pApplication.java
@@ -0,0 +1,13 @@
+package com.baby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyP2pApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyP2pApplication.class, args);
+    }
+
+}
