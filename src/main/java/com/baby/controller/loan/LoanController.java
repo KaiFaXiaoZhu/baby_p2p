78b0a05e5ec2bf4c023a8f6824a3913b8599154b
caokaihua <469048088@qq.com>
2020-02-08 10:40:04 +0800

    标的模块，投标模块都已完成

--- src/main/java/com/baby/controller/loan/LoanController.java
+++ src/main/java/com/baby/controller/loan/LoanController.java
@@ -73,23 +73,48 @@ public class LoanController {
                 bid.setBorrowId(borrowId);
                 JieK(bidList,borrow);
 
+                Integer totalAmount=0;               //本期还款总金额(单位：分)
+                Integer principal=0;                  //本期还款本金(单位：分)
+                Integer interest=0;                   //本期还款利息(单位：分)
+
                 borrow.setBorrowAmount(XianXiHouBeng.chu(borrow.getBorrowAmount().toString(),"100"));
                 for (int i=1;i<=borrow.getRepaymentMonth();i++){
+
+                    if (borrow.getRepaymentType()==1){//等额本息
+                        totalAmount=XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthPrincipalInterest(borrow.getBorrowAmount(), borrow.getYearRate()*0.01, borrow.getRepaymentMonth())+"","100");
+                        principal=XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthPrincipal(borrow.getBorrowAmount(), borrow.getYearRate()*0.01, borrow.getRepaymentMonth()).get(i).doubleValue()+"","100");
+                        interest=XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthInterest(borrow.getBorrowAmount(), borrow.getYearRate()*0.01, borrow.getRepaymentMonth()).get(i).doubleValue()+"","100");
+                    }else{//先息后本
+                        totalAmount=XianXiHouBeng.cheng(XianXiHouBeng.monthlyRepayment(new BigDecimal(borrow.getBorrowAmount()),new BigDecimal(borrow.getYearRate()*0.01),borrow.getRepaymentMonth(),i)+"","100");
+                        principal=i==12?XianXiHouBeng.cheng(borrow.getBorrowAmount().toString(),"100"):0;
+                        interest=XianXiHouBeng.cheng(XianXiHouBeng.getYueLiX(borrow.getBorrowAmount(),borrow.getYearRate()*0.01,borrow.getRepaymentMonth())+"","100");
+                    }
+
                     Calendar curr = Calendar.getInstance();
                     curr.set(Calendar.MONTH,curr.get(Calendar.MONTH)+borrow.getRepaymentMonth());
 
                     Repayment repayment=new Repayment(IdUtils.getUUID(),borrow.getId(),borrow.getBorrowUserId(),borrow.getTitle(),borrow.getBidDeadline(),curr.getTime(),
-                            XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthPrincipalInterest(borrow.getBorrowAmount(), borrow.getYearRate()*0.01, borrow.getRepaymentMonth())+"","100"),
-                            XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthPrincipal(borrow.getBorrowAmount(), borrow.getYearRate()*0.01, borrow.getRepaymentMonth()).get(i).doubleValue()+"","100"),
-                            XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthInterest(borrow.getBorrowAmount(), borrow.getYearRate()*0.01, borrow.getRepaymentMonth()).get(i).doubleValue()+"","100"),
-                            i,2,borrow.getBorrowType(),1,new Date());
+                            totalAmount,principal,interest, i,2,borrow.getBorrowType(),1,new Date());
                     int repaymentAdd=repaymentService.addRepayment(repayment);
                 }
 
                 List<Repayment> repaymentList=repaymentService.getByBorrowId(borrowId);
                 for (Repayment repayment:repaymentList){
                     for (Bid bid1:bidList){
+                        Integer bidAmount=XianXiHouBeng.chu(bid1.getBidAmount()+"","100");
                         RepaymentDetail repaymentDetail=new RepaymentDetail();
+                        if (borrow.getRepaymentType()==1){//等额本息
+                            totalAmount=XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthPrincipalInterest(bidAmount, bid1.getYearRate()*0.01, borrow.getRepaymentMonth())+"","100");
+                            principal=XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthPrincipal(bidAmount, bid1.getYearRate()*0.01, borrow.getRepaymentMonth()).get(repayment.getPeriod()).doubleValue()+"","100");
+                            interest=XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthInterest(bidAmount, bid1.getYearRate()*0.01, borrow.getRepaymentMonth()).get(repayment.getPeriod()).doubleValue()+"","100");
+                            repaymentDetail.setRepaymentType(borrow.getRepaymentType());
+                        }else{//先息后本
+                            totalAmount=XianXiHouBeng.cheng(XianXiHouBeng.monthlyRepayment(new BigDecimal(bid1.getBidAmount()),new BigDecimal(bid1.getYearRate()*0.01),borrow.getBorrowAmount(),repayment.getPeriod())+"","100");
+                            principal=repayment.getPeriod()==12?bid1.getBidAmount():0;
+                            interest=XianXiHouBeng.cheng(XianXiHouBeng.getYueLiX(bid1.getBidAmount(),bid1.getYearRate()*0.01,borrow.getRepaymentMonth())+"","100");
+                            repaymentDetail.setRepaymentType(borrow.getRepaymentType());
+                        }
+                        //封装
                         repaymentDetail.setId(IdUtils.getUUID());
                         repaymentDetail.setBidId(bid1.getId());
                         repaymentDetail.setBorrowId(bid1.getBorrowId());
@@ -97,13 +122,12 @@ public class LoanController {
                         repaymentDetail.setBorrowUserId(borrow.getBorrowUserId());
                         repaymentDetail.setBidUserId(bid1.getBidUserId());
                         repaymentDetail.setBorrowTitle(borrow.getTitle());
-                        repaymentDetail.setTotalAmount( XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthPrincipalInterest(bid1.getBidAmount()/100, bid1.getYearRate()*0.01, borrow.getRepaymentMonth())+"","100"));
-                        repaymentDetail.setPrincipal(XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthPrincipal(bid1.getBidAmount()/100, bid1.getYearRate()*0.01, borrow.getRepaymentMonth()).get(repayment.getPeriod()).doubleValue()+"","100"));
-                        repaymentDetail.setInterest( XianXiHouBeng.cheng(AverageCapitalPlusInterestUtils.getPerMonthInterest(bid1.getBidAmount()/100, bid1.getYearRate()*0.01, borrow.getRepaymentMonth()).get(repayment.getPeriod()).doubleValue()+"","100"));
+                        repaymentDetail.setTotalAmount(totalAmount);
+                        repaymentDetail.setPrincipal(principal);
+                        repaymentDetail.setInterest(interest);
                         repaymentDetail.setPeriod(repayment.getPeriod());
                         repaymentDetail.setDeadline(repayment.getDeadline());
                         repaymentDetail.setRepaymentTime(repayment.getRepaymentTime());
-                        repaymentDetail.setRepaymentType(1);
                         repaymentDetail.setCreateTime(new Date());
                         int RepaymentDetailAdd=repaymentDetailService.addRepaymentDetail(repaymentDetail);
                     }
